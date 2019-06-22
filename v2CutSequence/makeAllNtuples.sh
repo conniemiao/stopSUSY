366901ae37d38a3b2e72087f92ce53516920e09d
Connie Miao <conniem@princeton.edu>
2019-06-22 19:08:23 +0200

    made plotSusyBkgd+Sig accept multiple plotVars to plot multiple things faster

--- v2CutSequence/makeAllNtuples.sh
+++ v2CutSequence/makeAllNtuples.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# NOTE: THIS DOESN'T WORK. LOL. Right now just makes the sigs ntuples.
 # for each setting combo for creating bkgd and sig ntuples, creates a condorsub
 # file for it and then submits it.
 
@@ -21,21 +22,21 @@ echo
 echo "python makeNtupleSigs.py 1 1 1 1"
 python makeNtupleSigs.py 1 1 1 1
 
-echo
-echo "python makeNtupleBkgd.py 1 0 0 0"
-python makeNtupleBkgd.py 1 0 0 0
-echo
-echo "python makeNtupleBkgd.py 1 1 0 0"
-python makeNtupleBkgd.py 1 1 0 0
-echo
-echo "python makeNtupleBkgd.py 1 0 1 0"
-python makeNtupleBkgd.py 1 0 1 0
-echo
-echo "python makeNtupleBkgd.py 1 1 1 0"
-python makeNtupleBkgd.py 1 1 1 0
-echo
-echo "python makeNtupleBkgd.py 1 0 1 1"
-python makeNtupleBkgd.py 1 0 1 1
-echo
-echo "python makeNtupleBkgd.py 1 1 1 1"
-python makeNtupleBkgd.py 1 1 1 1
+# echo
+# echo "python makeNtupleBkgd.py 1 0 0 0"
+# python makeNtupleBkgd.py 1 0 0 0
+# echo
+# echo "python makeNtupleBkgd.py 1 1 0 0"
+# python makeNtupleBkgd.py 1 1 0 0
+# echo
+# echo "python makeNtupleBkgd.py 1 0 1 0"
+# python makeNtupleBkgd.py 1 0 1 0
+# echo
+# echo "python makeNtupleBkgd.py 1 1 1 0"
+# python makeNtupleBkgd.py 1 1 1 0
+# echo
+# echo "python makeNtupleBkgd.py 1 0 1 1"
+# python makeNtupleBkgd.py 1 0 1 1
+# echo
+# echo "python makeNtupleBkgd.py 1 1 1 1"
+# python makeNtupleBkgd.py 1 1 1 1
--- v2CutSequence/plotSusyBkgd+Sig.py
+++ v2CutSequence/plotSusyBkgd+Sig.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# NOTE: NEEDS 4 CMD LINE ARGS with values {0 (false) or 1 (true)}: 
-# testMode, cutMode, findingSameFlavor, muPreference, plotVar
+# NOTE: NEEDS >= 5 CMD LINE ARGS with values {0 (false) or 1 (true)}: 
+# testMode, cutMode, findingSameFlavor, muPreference, plotVar1, plotVar2, ...
 # Draws 1D hist for data for some variable, for the summed bkgd data and for each 
 # of the signal files.
 # Uses the root files outputted by makeNtupleBkgd.py and makeNtupleSigs.py
@@ -9,15 +9,16 @@
 
 import sys
 from ROOT import TFile, TTree, TH1D, TCanvas, TLorentzVector, TImage, TLegend
-from ROOT import gSystem, gStyle
+from ROOT import gSystem, gStyle, gROOT, kTRUE
 import numpy as np
 
-assert len(sys.argv) == 6, "need 5 command line args: testMode{0,1}, cutMode{0,1}, findingSameFlavor{0,1}, muPreference{0,1}, plotVar"
-plotVar = sys.argv[5]
+
+assert len(sys.argv) >= 6, "need at least 5 command line args: testMode{0,1}, cutMode{0,1}, findingSameFlavor{0,1}, muPreference{0,1}, plotVar1, plotVar2, ..."
+plotVarArr = sys.argv[5:]
 
 plotSettings = { # [nBins,xMin,xMax,listForm]
         "lep1_pt":[100,0,400,False],
-        "lep1_eta":[100,-3,3,False],
+        "lep1_eta":[100,-4,4,False],
         "lep1_phi":[100,-4,4,False],
         "lep1_relIso":[10,0,0.2,False],
         "lep2_pt":[100,0,400,False],
@@ -34,10 +35,11 @@ plotSettings = { # [nBins,xMin,xMax,listForm]
         "mtlep1":[100,0,500,False],
         "mtlep2":[100,0,500,False],
         "met_pt":[100,0,500,False],
-        "met_phi":[100,-4,-4,False],
+        "met_phi":[100,-4,4,False],
         }
 
-assert (plotVar in plotSettings), "invalid plotVar"
+for plotVar in plotVarArr:
+    assert (plotVar in plotSettings), "invalid plotVar %s" % plotVar
 
 # Determining adr of bkgd and sig ntuples.
 # limits the number of events and files to loop over
@@ -85,27 +87,37 @@ else:
     sigsNtupleAdr += "_withcuts.root"
     bkgdNtupleAdr += "_withcuts.root"
 
-print "Plotting",plotVar,"from",bkgdNtupleAdr,"and",sigsNtupleAdr
+print "Plotting",str(plotVarArr),"from",bkgdNtupleAdr,"and",sigsNtupleAdr
 
 numSigFiles = int(sigsNtupleAdr[48:50])
-nBins = plotSettings[plotVar][0]
-# if not testMode and nBins > 20: nBins = nBins * 5
-xMin = plotSettings[plotVar][1]
-xMax = plotSettings[plotVar][2]
-listForm = plotSettings[plotVar][3] # only for some of the jet variables 
 
-binwidth = (xMax - xMin)/nBins # include overflow bin
-hBkgd = TH1D(plotVar + "_bkgd", plotVar + "_bkgd", nBins + 1, \
-        xMin, xMax + binwidth)
+canvasDict = {}
+legendDict = {}
+
+hBkgdDict = {} 
+if not testMode:
+    gROOT.SetBatch(kTRUE) # prevent displaying canvases
+for plotVar in plotVarArr: # add an entry to the plotVar:hist dictionary
+    nBins = plotSettings[plotVar][0]
+    # if not testMode and nBins > 20: nBins = nBins * 5
+    xMin = plotSettings[plotVar][1]
+    xMax = plotSettings[plotVar][2]
+    binwidth = (xMax - xMin)/nBins # include overflow bin
+    hBkgd = TH1D(plotVar + "_bkgd", plotVar + "_bkgd", \
+            nBins + 1, xMin, xMax + binwidth)
+    hBkgd.SetDirectory(0) # necessary to keep hist from closing
+    hBkgdDict.update({plotVar:hBkgd})
+    c = TCanvas("c_"+plotVar,"Plot",10,20,1000,700)
+    canvasDict.update({plotVar:c})
+    legendDict.update({plotVar:TLegend(.70,.75,.90,.90)})
 lumi = 3000000 # luminosity = 3000 /fb = 3,000,000 /pb
 
-c1 = TCanvas("c1","Plot",10,20,1000,700)
 gStyle.SetOptStat(0) # don't show any stats
 
 #--------------------------------------------------------------------------------#
 # *************** Filling bkgd data summed together  ************
-print "Plotting", plotVar, "from background."
-xsec = 67.75 # production cross section
+print "Plotting from background."
+xsec = 67.75 # ttbar production cross section
 bkgdFile = TFile.Open(bkgdNtupleAdr, "READ")
 tBkgd = bkgdFile.Get("tBkgd")
 
@@ -115,53 +127,56 @@ assert nentries > 0, "You have no events in your tree..."
 
 for count, event in enumerate(tBkgd):
     if count % 500000 == 0: print("count={0:d}".format(count))
-    val = getattr(event, plotVar)
-    if listForm:
-        numjets = event.njets
-        val = np.reshape(val, 20)
-        for j in range(numjets):
-            jetval = val[j]
-            if jetval <= xMax:
-                hBkgd.Fill(jetval, 1)
+    for plotVar in plotVarArr:
+        hBkgd = hBkgdDict[plotVar]
+        val = getattr(event, plotVar)
+        listForm = plotSettings[plotVar][3]
+        if listForm:
+            numjets = event.njets
+            val = np.reshape(val, 20)
+            for j in range(numjets):
+                jetval = val[j]
+                if jetval <= xMax:
+                    hBkgd.Fill(jetval, 1)
+                else: # overflow
+                    hBkgd.Fill(xMax + binwidth/2, 1)
+        else:
+            if val <= xMax:
+                hBkgd.Fill(val, 1)
             else: # overflow
                 hBkgd.Fill(xMax + binwidth/2, 1)
-    else:
-        if val <= xMax:
-            hBkgd.Fill(val, 1)
-        else: # overflow
-            hBkgd.Fill(xMax + binwidth/2, 1)
-hBkgd.Sumw2()
-
-# rebinning
-# print hBkgd.GetBinError((xMax+xMin)/2)/hBkgd.GetSumOfWeights()
-# while hBkgd.GetBinError((xMax+xMin)/2) > 0.1*hBkgd.GetSumOfWeights():
-#     hBkgd.Rebin(2)
-#     print rebinned
-
-title = plotVar + " ("+bkgdNtupleAdr[-18:-14]+", normalized to 3000 /fb)"
-title += ", "+bkgdNtupleAdr[-13:-5]
-hBkgd.SetTitle(title)
-hBkgd.GetXaxis().SetTitle(plotVar+" [GeV]")
-hBkgd.GetYaxis().SetTitle("Number of Events")
-hBkgd.Scale(xsec*lumi/hBkgd.GetSumOfWeights())
-hBkgd.SetMinimum(1)
-hBkgd.SetMaximum(10**12)
-hBkgd.SetLineColor(1) # black
-# hBkgd.SetStats(0)
-hBkgd.Draw("hist")
-c1.Update()
+for plotVar in plotVarArr:
+    c = canvasDict[plotVar]
+    c.cd()
+    hBkgd = hBkgdDict[plotVar]
+    hBkgd.Sumw2()
+    title = plotVar + " ("+bkgdNtupleAdr[-18:-14]+", normalized to 3000 /fb)"
+    title += ", "+bkgdNtupleAdr[-13:-5]
+    hBkgd.SetTitle(title)
+    hBkgd.GetXaxis().SetTitle(plotVar+" [GeV]")
+    hBkgd.GetYaxis().SetTitle("Number of Events")
+    hBkgd.Scale(xsec*lumi/hBkgd.GetSumOfWeights())
+    hBkgd.SetMinimum(1)
+    hBkgd.SetMaximum(10**12)
+    hBkgd.SetLineColor(1) # black
+    legend = legendDict[plotVar]
+    legend.AddEntry(hBkgd, plotVar+"_bkgd")
+    hBkgd.Draw("hist")
 bkgdFile.Close()
 
 #--------------------------------------------------------------------------------#
 # *************** Filling each signal data in a separate hist  ************
-print "Plotting " + plotVar + " from signal."
+print "Plotting from signal."
 sigDataListFile = open("sig_SingleStop_files")
 
 coloropts = [2,4,3,6,7,9,28,46] # some good colors for lines
 markeropts = [1,20,21,22,23] # some good marker styles for lines
 linestyleopts = [1,2,3,7,9] # some good styles for lines
 
-hSigArr = [] # one hist for each signal file 
+hSigArrDict = {}
+for plotVar in plotVarArr: # add an entry to the plotVar:hist dictionary
+    hSigArrDict.update({plotVar:[]})
+
 for fileNum, line in enumerate(sigDataListFile):
     if fileNum + 1 > numSigFiles: break
 
@@ -176,71 +191,87 @@ for fileNum, line in enumerate(sigDataListFile):
     print("nentries={0:d}".format(nentries))
     assert nentries > 0, "You have no events in your tree..."
 
-    hSigArr.append(TH1D(plotVar + "_sig_" + filename, plotVar + "_sig_" + \
-            filename[19:24], nBins + 1, xMin, xMax + binwidth))
+    for plotVar in plotVarArr:
+        nBins = plotSettings[plotVar][0]
+        # if not testMode and nBins > 20: nBins = nBins * 5
+        xMin = plotSettings[plotVar][1]
+        xMax = plotSettings[plotVar][2]
+        binwidth = (xMax - xMin)/nBins # include overflow bin
+        hSigArr = hSigArrDict[plotVar]  # one hist for each signal file
+        hSig = TH1D(plotVar + "_sig_" + filename, plotVar + "_sig_" + \
+                filename[21:31], nBins + 1, xMin, xMax + binwidth)
+        hSig.SetDirectory(0)
+        hSigArr.append(hSig)
     
     for count, event in enumerate(tSig):
         if count % 500000 == 0: print("count={0:d}".format(count))
-        val = getattr(event, plotVar)
-        if listForm:
-            numjets = event.njets
-            val = np.reshape(val, 20)
-            for j in range(numjets):
-                jetval = val[j]
-                if jetval <= xMax:
-                    hSigArr[fileNum].Fill(jetval, 1)
+        for plotVar in plotVarArr:
+            hSig = hSigArrDict[plotVar][fileNum]
+            val = getattr(event, plotVar)
+            listForm = plotSettings[plotVar][3]
+            if listForm:
+                numjets = event.njets
+                val = np.reshape(val, 20)
+                for j in range(numjets):
+                    jetval = val[j]
+                    if jetval <= xMax:
+                        hSig.Fill(jetval, 1)
+                    else: # overflow
+                        hSig.Fill(xMax + binwidth/2, 1)
+            else:
+                if val <= xMax:
+                    hSig.Fill(val, 1)
                 else: # overflow
-                    hSigArr[fileNum].Fill(xMax + binwidth/2, 1)
-        else:
-            if val <= xMax:
-                hSigArr[fileNum].Fill(val, 1)
-            else: # overflow
-                hSigArr[fileNum].Fill(xMax + binwidth/2, 1)
-
-    hSigArr[fileNum].SetDirectory(0) # necessary to keep hist from closing
+                    hSig.Fill(xMax + binwidth/2, 1)
 
     hcolor = coloropts[fileNum % len(coloropts)]
-    hSigArr[fileNum].SetLineColor(hcolor) 
     hmarkerstyle = markeropts[(fileNum/len(coloropts)) % len(markeropts)]
-    hSigArr[fileNum].SetMarkerStyle(hmarkerstyle)
-    hSigArr[fileNum].SetMarkerColor(hcolor)
-    hlinestyle = linestyleopts[(fileNum/len(coloropts)/len(markeropts)) % \
-            len(linestyleopts)]
-    hSigArr[fileNum].SetLineStyle(hlinestyle)
 
-    hSigArr[fileNum].Sumw2()
-    hSigArr[fileNum].Scale(xsec * lumi / hSigArr[fileNum].GetSumOfWeights())
-    hSigArr[fileNum].SetMinimum(1)
-    hSigArr[fileNum].SetMaximum(10**12)
-    hSigArr[fileNum].Draw("hist same") # same pad
-    c1.Update()
+    for plotVar in plotVarArr:
+        c = canvasDict[plotVar]
+        c.cd()
+        hSig = hSigArrDict[plotVar][fileNum]
+        hSig.SetLineColor(hcolor) 
+        hSig.SetMarkerStyle(hmarkerstyle)
+        hSig.SetMarkerColor(hcolor)
+        hlinestyle = linestyleopts[(fileNum/len(coloropts)/len(markeropts)) % \
+                len(linestyleopts)]
+        hSig.SetLineStyle(hlinestyle)
+
+        hSig.Sumw2()
+        hSig.Scale(xsec * lumi / hSig.GetSumOfWeights())
+        hSig.SetMinimum(1)
+        hSig.SetMaximum(10**12)
+        legend = legendDict[plotVar]
+        legend.AddEntry(hSig, hSig.GetTitle())
+        hSig.Draw("hist same") # same pad
     sigFile.Close()
 
 
 #--------------------------------------------------------------------------------#
 # *************** Wrap up. *******************
 
-legend = TLegend(.70,.75,.90,.90)
-legend.AddEntry(hBkgd, plotVar + "_bkgd")
-legend.SetTextSize(0.02)
-for fileNum, h in enumerate(hSigArr):
-    legend.AddEntry(hSigArr[fileNum], hSigArr[fileNum].GetTitle())
-legend.Draw("same")
-
-c1.SetLogy()
-c1.Update()
+for plotVar in plotVarArr:
+    c = canvasDict[plotVar]
+    c.cd()
+    legend = legendDict[plotVar]
+    legend.SetTextSize(0.02)
+    legend.Draw("same")
+    c.SetLogy()
+    c.Update()
 
 if testMode:
     print "Done. Press enter to finish."
     raw_input()
 else:
-    imgName = "/afs/cern.ch/user/c/cmiao/private/CMSSW_9_4_9/s2019_SUSY/"+\
-            "plots/v2CutSequence/"+plotVar+"_"+l1Flav[:2]+l2Flav[:2]+"_"+\
-            sigsNtupleAdr[-13:-5]+".png"
-    print "Saving image", imgName
     gSystem.ProcessEvents()
-    img = TImage.Create()
-    img.FromPad(c1)
-    img.WriteImage(imgName)
+    for plotVar in plotVarArr:
+        imgName = "/afs/cern.ch/user/c/cmiao/private/CMSSW_9_4_9/s2019_SUSY/"+\
+                "plots/v2CutSequence/"+plotVar+"_"+l1Flav[:2]+l2Flav[:2]+"_"+\
+                sigsNtupleAdr[-13:-5]+".png"
+        print "Saving image", imgName
+        img = TImage.Create()
+        img.FromPad(canvasDict[plotVar])
+        img.WriteImage(imgName)
     print "Done."
 
