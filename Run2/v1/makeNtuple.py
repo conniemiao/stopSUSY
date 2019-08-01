0144d73d22ba2789e419477a30bf054cc9ba6a55
Connie Miao <conniem@princeton.edu>
2019-08-01 19:21:45 +0200

    added a ton of new branch vars for ntupling for leptons; saving only indices of selected leptons rather than a repeat of all info; added some LHE and PV vars

--- Run2/v1/makeNtuple.py
+++ Run2/v1/makeNtuple.py
@@ -105,31 +105,58 @@ Muon_eta = np.zeros(20, dtype=np.float32)
 Muon_phi = np.zeros(20, dtype=np.float32)
 Muon_relIso = np.zeros(20, dtype=np.float32)
 Muon_charge = np.zeros(20, dtype=np.float32)
+Muon_dxy = np.zeros(20, dtype=np.float32)
+Muon_dz = np.zeros(20, dtype=np.float32)
+Muon_mass = np.zeros(20, dtype=np.float32)
+Muon_miniPFRelIso_all = np.zeros(20, dtype=np.float32)
+Muon_inTimeMuon = np.zeros(20, dtype=np.int32) # read as bool
+Muon_ip3d = np.zeros(20, dtype=np.float32)
+Muon_isGlobal = np.zeros(20, dtype=np.int32) # read as bool
+Muon_isPFcand = np.zeros(20, dtype=np.int32) # read as bool
+Muon_isTracker = np.zeros(20, dtype=np.int32) # read as bool
+Muon_jetIdx = np.zeros(20, dtype=np.int32)
+Muon_pdgId = np.zeros(20, dtype=np.int32)
+Muon_looseId = np.zeros(20, dtype=np.int32) # read as bool
+Muon_mediumId = np.zeros(20, dtype=np.int32) # read as bool
+Muon_mediumPromptId = np.zeros(20, dtype=np.int32) # read as bool
+Muon_mvaId = np.zeros(20, dtype=np.int32)
+Muon_tightId = np.zeros(20, dtype=np.int32) # read as bool
+Muon_genPartFlav = np.zeros(20, dtype=np.int32)
+Muon_genPartIdx = np.zeros(20, dtype=np.int32)
 Muon_mt = np.zeros(20, dtype=np.float32)
+
 nElectron = array('i',[0])
 Electron_pt = np.zeros(20, dtype=np.float32)
 Electron_eta = np.zeros(20, dtype=np.float32)
 Electron_phi = np.zeros(20, dtype=np.float32)
 Electron_relIso = np.zeros(20, dtype=np.float32)
 Electron_charge = np.zeros(20, dtype=np.float32)
+Electron_dxy = np.zeros(20, dtype=np.float32)
+Electron_dz = np.zeros(20, dtype=np.float32)
+Electron_mass = np.zeros(20, dtype=np.float32)
+Electron_miniPFRelIso_all = np.zeros(20, dtype=np.float32)
+# Electron_inTimeMuon = np.zeros(20, dtype=np.int32) # read as bool
+Electron_ip3d = np.zeros(20, dtype=np.float32)
+# Electron_isGlobal = np.zeros(20, dtype=np.int32) # read as bool
+Electron_isPFcand = np.zeros(20, dtype=np.int32) # read as bool
+# Electron_isTracker = np.zeros(20, dtype=np.int32) # read as bool
+Electron_jetIdx = np.zeros(20, dtype=np.int32)
+Electron_pdgId = np.zeros(20, dtype=np.int32)
+# Electron_looseId = np.zeros(20, dtype=np.int32) # read as bool
+# Electron_mediumId = np.zeros(20, dtype=np.int32) # read as bool
+# Electron_mediumPromptId = np.zeros(20, dtype=np.int32) # read as bool
+# Electron_mvaId = np.zeros(20, dtype=np.int32)
+# Electron_tightId = np.zeros(20, dtype=np.int32) # read as bool
+Electron_genPartFlav = np.zeros(20, dtype=np.int32)
+Electron_genPartIdx = np.zeros(20, dtype=np.int32)
 Electron_mt = np.zeros(20, dtype=np.float32)
+
 found3rdLept = array('i',[0])
-lep1_isMu = array('i',[0]) # True (1) if muon, False (0) if electron
+lep1_isMu = array('i',[0]) # read as bool
 lep1_index = array('i',[0])
-lep1_pt = array('f',[0.])
-lep1_eta = array('f',[0.])
-lep1_phi = array('f',[0.])
-lep1_relIso = array('f',[0.])
-lep1_charge = array('f',[0.])
-lep1_mt = array('f',[0.])
-lep2_isMu = array('i',[0]) # True (1) if muon, False (0) if electron
+lep2_isMu = array('i',[0]) # read as bool
 lep2_index = array('i',[0])
-lep2_pt = array('f',[0.])
-lep2_eta = array('f',[0.])
-lep2_phi = array('f',[0.])
-lep2_relIso = array('f',[0.])
-lep2_charge = array('f',[0.])
-lep2_mt = array('f',[0.])
+
 Jet_pt = np.zeros(20, dtype=np.float32)
 Jet_eta = np.zeros(20, dtype=np.float32)
 Jet_phi = np.zeros(20, dtype=np.float32)
@@ -141,15 +168,35 @@ nJet = array('i',[0])
 nbtag = array('i',[0])
 nbtagLoose = array('i',[0])
 nbtagTight = array('i',[0])
-met_pt = array('f',[0.])
-met_phi = array('f',[0.])
+
+MET_pt = array('f',[0.])
+MET_phi = array('f',[0.])
+MET_significance = array('f',[0.])
+MET_sumEt = array('f',[0.])
 mt_tot = array('f',[0.])
 mt_sum = array('f',[0.])
 m_eff = array('f',[0.])
-if isData:
-    run = array('i',[0])
-    luminosityBlock = array('i',[0])
-else: genWeight = array('f',[0.])
+
+LHE_HT = array('f',[0.])
+LHE_HTIncoming = array('f',[0.])
+LHE_Njets = array('i',[0])
+LHE_Nb = array('i',[0])
+LHE_Nuds = array('i',[0])
+LHE_Nglu = array('i',[0])
+LHE_NpNLO = array('i',[0])
+LHE_NpLO = array('i',[0])
+# LHEPart_pt = array('f',[0.])
+# LHEPart_eta = array('f',[0.])
+# LHEPart_phi = array('f',[0.])
+# LHEPart_mass = array('f',[0.])
+# LHEPart_pdgId = array('i',[0])
+LHEWeight_originalXWGTUP = array('f',[0.])
+
+PV_npvs = array('i',[0])
+PV_npvsGood = array('i',[0])
+luminosityBlock = array('i',[0])
+run = array('i',[0])
+genWeight = array('f',[0.])
 
 #--------------------------------------------------------------------------------#
 start_time = time.time()
@@ -184,38 +231,56 @@ for line in redirectorFile:
 
     # SET UP THE OUTPUT TREE
     Events = TTree("Events", "SUSY stop cut events")
-    Events.Branch("nMuon", nMuon, "nMuon/i")
+    Events.Branch("nMuon", nMuon, "nMuon/I")
     Events.Branch("Muon_pt", Muon_pt, "Muon_pt[20]/F")
     Events.Branch("Muon_eta", Muon_eta, "Muon_eta[20]/F")
     Events.Branch("Muon_phi", Muon_phi, "Muon_phi[20]/F")
     Events.Branch("Muon_relIso", Muon_relIso, "Muon_relIso[20]/F")
     Events.Branch("Muon_charge", Muon_charge, "Muon_charge[20]/F")
+    Events.Branch("Muon_dxy", Muon_dxy, "Muon_dxy[20]/F")
+    Events.Branch("Muon_dz", Muon_dz, "Muon_dz[20]/F")
+    Events.Branch("Muon_mass", Muon_mass, "Muon_mass[20]/F")
+    Events.Branch("Muon_miniPFRelIso_all", Muon_miniPFRelIso_all, "Muon_miniPFRelIso_all[20]/F")
+    Events.Branch("Muon_inTimeMuon", Muon_inTimeMuon, "Muon_inTimeMuon[20]/I")
+    Events.Branch("Muon_ip3d", Muon_ip3d, "Muon_ip3d[20]/F")
+    Events.Branch("Muon_isGlobal", Muon_isGlobal, "Muon_isGlobal[20]/I")
+    Events.Branch("Muon_isPFcand", Muon_isPFcand, "Muon_isPFcand[20]/I")
+    Events.Branch("Muon_isTracker", Muon_isTracker, "Muon_isTracker[20]/I")
+    Events.Branch("Muon_jetIdx", Muon_jetIdx, "Muon_jetIdx[20]/I")
+    Events.Branch("Muon_pdgId", Muon_pdgId, "Muon_pdgId[20]/I")
+    Events.Branch("Muon_looseId", Muon_looseId, "Muon_looseId[20]/I")
+    Events.Branch("Muon_mediumId", Muon_mediumId, "Muon_mediumId[20]/I")
+    Events.Branch("Muon_mediumPromptId", Muon_mediumPromptId, "Muon_mediumPromptId[20]/I")
+    Events.Branch("Muon_mvaId", Muon_mvaId, "Muon_mvaId[20]/I")
+    Events.Branch("Muon_tightId", Muon_tightId, "Muon_tightId[20]/I")
+    Events.Branch("Muon_genPartFlav", Muon_genPartFlav, "Muon_genPartFlav[20]/I")
+    Events.Branch("Muon_genPartFlav", Muon_genPartFlav, "Muon_genPartFlav[20]/I")
+    Events.Branch("Muon_genPartIdx", Muon_genPartIdx, "Muon_genPartIdx[20]/I")
     Events.Branch("Muon_mt", Muon_mt, "Muon_mt[20]/F")
-    Events.Branch("nElectron", nElectron, "nElectron/i")
+    Events.Branch("nElectron", nElectron, "nElectron/I")
     Events.Branch("Electron_pt", Electron_pt, "Electron_pt[20]/F")
     Events.Branch("Electron_eta", Electron_eta, "Electron_eta[20]/F")
     Events.Branch("Electron_phi", Electron_phi, "Electron_phi[20]/F")
     Events.Branch("Electron_relIso", Electron_relIso, "Electron_relIso[20]/F")
     Events.Branch("Electron_charge", Electron_charge, "Electron_charge[20]/F")
+    Events.Branch("Electron_dxy", Electron_dxy, "Electron_dxy[20]/F")
+    Events.Branch("Electron_dz", Electron_dz, "Electron_dz[20]/F")
+    Events.Branch("Electron_mass", Electron_mass, "Electron_mass[20]/F")
+    Events.Branch("Electron_miniPFRelIso_all", Electron_miniPFRelIso_all, "Electron_miniPFRelIso_all[20]/F")
+    Events.Branch("Electron_ip3d", Electron_ip3d, "Electron_ip3d[20]/F")
+    Events.Branch("Electron_isPFcand", Electron_isPFcand, "Electron_isPFcand[20]/I")
+    Events.Branch("Electron_jetIdx", Electron_jetIdx, "Electron_jetIdx[20]/I")
+    Events.Branch("Electron_pdgId", Electron_pdgId, "Electron_pdgId[20]/I")
+    Events.Branch("Electron_genPartFlav", Electron_genPartFlav, "Electron_genPartFlav[20]/I")
+    Events.Branch("Electron_genPartFlav", Electron_genPartFlav, "Electron_genPartFlav[20]/I")
+    Events.Branch("Electron_genPartIdx", Electron_genPartIdx, "Electron_genPartIdx[20]/I")
     Events.Branch("Electron_mt", Electron_mt, "Electron_mt[20]/F")
-    Events.Branch("found3rdLept", found3rdLept, "found3rdLept/i")
-    Events.Branch("lep1_isMu", lep1_isMu, "lep1_isMu/i")
-    Events.Branch("lep1_index", lep1_index, "lep1_index/i")
-    Events.Branch("lep1_pt", lep1_pt, "lep1_pt/F")
-    Events.Branch("lep1_eta", lep1_eta, "lep1_eta/F")
-    Events.Branch("lep1_phi", lep1_phi, "lep1_phi/F")
-    Events.Branch("lep1_relIso", lep1_relIso, "lep1_relIso/F")
-    Events.Branch("lep1_charge", lep1_charge, "lep1_charge/F")
-    Events.Branch("lep1_mt", lep1_mt, "lep1_mt/F")
-    Events.Branch("lep2_isMu", lep2_isMu, "lep2_isMu/i")
-    Events.Branch("lep2_index", lep2_index, "lep2_index/i")
-    Events.Branch("lep2_pt", lep2_pt, "lep2_pt/F")
-    Events.Branch("lep2_eta", lep2_eta, "lep2_eta/F")
-    Events.Branch("lep2_phi", lep2_phi, "lep2_phi/F")
-    Events.Branch("lep2_relIso", lep2_relIso, "lep2_relIso/F")
-    Events.Branch("lep2_charge", lep2_charge, "lep2_charge/F")
-    Events.Branch("lep2_mt", lep2_mt, "lep2_mt/F")
-    Events.Branch("nJet", nJet, "nJet/i")
+    Events.Branch("found3rdLept", found3rdLept, "found3rdLept/I")
+    Events.Branch("lep1_isMu", lep1_isMu, "lep1_isMu/I")
+    Events.Branch("lep1_index", lep1_index, "lep1_index/I")
+    Events.Branch("lep2_isMu", lep2_isMu, "lep2_isMu/I")
+    Events.Branch("lep2_index", lep2_index, "lep2_index/I")
+    Events.Branch("nJet", nJet, "nJet/I")
     Events.Branch("Jet_pt", Jet_pt, "Jet_pt[20]/F")
     Events.Branch("Jet_eta", Jet_eta, "Jet_eta[20]/F")
     Events.Branch("Jet_phi", Jet_phi, "Jet_phi[20]/F")
@@ -223,19 +288,34 @@ for line in redirectorFile:
     # Events.Branch("Jet_flavour", Jet_flavour, "Jet_flavour/F")
     Events.Branch("dR_lep1_jet", dR_lep1_jet, "dR_lep1_jet/F")
     Events.Branch("dR_lep2_jet", dR_lep2_jet, "dR_lep2_jet/F")
-    Events.Branch("nbtag", nbtag, "nbtag/i")
-    Events.Branch("nbtagLoose", nbtagLoose, "nbtagLoose/i")
-    Events.Branch("nbtagTight", nbtagTight, "nbtagTight/i")
-    Events.Branch("met_pt", met_pt, "met_pt/F")
-    Events.Branch("met_phi", met_phi, "met_phi/F")
+    Events.Branch("nbtag", nbtag, "nbtag/I")
+    Events.Branch("nbtagLoose", nbtagLoose, "nbtagLoose/I")
+    Events.Branch("nbtagTight", nbtagTight, "nbtagTight/I")
+    Events.Branch("MET_pt", MET_pt, "MET_pt/F")
+    Events.Branch("MET_phi", MET_phi, "MET_phi/F")
+    Events.Branch("MET_significance", MET_significance, "MET_significance/F")
+    Events.Branch("MET_sumEt", MET_sumEt, "MET_sumEt/F")
     Events.Branch("mt_tot", mt_tot, "mt_tot/F")
     Events.Branch("mt_sum", mt_sum, "mt_sum/F")
     Events.Branch("m_eff", m_eff, "m_eff/F")
-    if isData:
-        Events.Branch("run", run, "run/i")
-        Events.Branch("luminosityBlock", luminosityBlock, "luminosityBlock/i")
-    else:
-        Events.Branch("genWeight", genWeight, "genWeight/F")
+    Events.Branch("LHE_HT", LHE_HT, "LHE_HT/F")
+    Events.Branch("LHE_HTIncoming", LHE_HTIncoming, "LHE_HTIncoming/F")
+    Events.Branch("LHE_Njets", LHE_Njets, "LHE_Njets/I")
+    Events.Branch("LHE_Nb", LHE_Nb, "LHE_Nb/I")
+    Events.Branch("LHE_Nuds", LHE_Nuds, "LHE_Nuds/I")
+    Events.Branch("LHE_Nglu", LHE_Nglu, "LHE_Nglu/I")
+    Events.Branch("LHE_NpNLO", LHE_NpNLO, "LHE_NpNLO/I")
+    # Events.Branch("LHEPart_pt", LHEPart_pt, "LHEPart_pt/F")
+    # Events.Branch("LHEPart_eta", LHEPart_eta, "LHEPart_eta/F")
+    # Events.Branch("LHEPart_phi", LHEPart_phi, "LHEPart_phi/F")
+    # Events.Branch("LHEPart_mass", LHEPart_mass, "LHEPart_mass/F")
+    # Events.Branch("LHEPart_pdgId", LHEPart_pdgId, "LHEPart_pdgId/I")
+    Events.Branch("LHEWeight_originalXWGTUP", LHEWeight_originalXWGTUP, "LHEWeight_originalXWGTUP/F")
+    Events.Branch("PV_npvs", PV_npvs, "PV_npvs/I")
+    Events.Branch("PV_npvsGood", PV_npvsGood, "PV_npvsGood/I")
+    Events.Branch("luminosityBlock", luminosityBlock, "luminosityBlock/I")
+    Events.Branch("run", run, "run/I")
+    Events.Branch("genWeight", genWeight, "genWeight/F")
 
     if not isData:
         hGenweights = TH1D("genWeights","genWeights",1,-0.5,0.5)
@@ -307,6 +387,24 @@ for line in redirectorFile:
                 Muon_phi[i] = list(event.Muon_phi)[i]
                 Muon_relIso[i] = list(event.Muon_pfRelIso04_all)[i]
                 Muon_charge[i] = list(event.Muon_charge)[i]
+                Muon_dxy[i] = list(event.Muon_dxy)[i]
+                Muon_dz[i] = list(event.Muon_dz)[i]
+                Muon_mass[i] = list(event.Muon_mass)[i]
+                Muon_miniPFRelIso_all[i] = list(event.Muon_miniPFRelIso_all)[i]
+                Muon_inTimeMuon[i] = list(event.Muon_inTimeMuon)[i]
+                Muon_ip3d[i] = list(event.Muon_ip3d)[i]
+                Muon_isGlobal[i] = list(event.Muon_isGlobal)[i]
+                Muon_isPFcand[i] = list(event.Muon_isPFcand)[i]
+                Muon_isTracker[i] = list(event.Muon_isTracker)[i]
+                Muon_jetIdx[i] = list(event.Muon_jetIdx)[i]
+                Muon_pdgId[i] = list(event.Muon_pdgId)[i]
+                Muon_looseId[i] = list(event.Muon_looseId)[i]
+                Muon_mediumId[i] = list(event.Muon_mediumId)[i]
+                Muon_mediumPromptId[i] = list(event.Muon_mediumPromptId)[i]
+                Muon_mvaId[i] = ord(list(event.Muon_mvaId)[i]) # UChar_t conversion
+                Muon_tightId[i] = list(event.Muon_tightId)[i]
+                Muon_genPartFlav[i] = ord(list(event.Muon_genPartFlav)[i])
+                Muon_genPartIdx[i] = list(event.Muon_genPartIdx)[i]
                 Muon_mt[i] = sqrt(2 * Muon_pt[i] * event.MET_pt * \
                         (1 - cos(Muon_phi[i] - event.MET_phi)))
     
@@ -318,30 +416,32 @@ for line in redirectorFile:
                 Electron_phi[i] = list(event.Electron_phi)[i]
                 Electron_relIso[i] = list(event.Electron_pfRelIso03_all)[i]
                 Electron_charge[i] = list(event.Electron_charge)[i]
+                Electron_dxy[i] = list(event.Electron_dxy)[i]
+                Electron_dz[i] = list(event.Electron_dz)[i]
+                Electron_mass[i] = list(event.Electron_mass)[i]
+                Electron_miniPFRelIso_all[i] = list(event.Electron_miniPFRelIso_all)[i]
+                # Electron_inTimeMuon[i] = list(event.Electron_inTimeMuon)[i]
+                Electron_ip3d[i] = list(event.Electron_ip3d)[i]
+                # Electron_isGlobal[i] = list(event.Electron_isGlobal)[i]
+                Electron_isPFcand[i] = list(event.Electron_isPFcand)[i]
+                # Electron_isTracker[i] = list(event.Electron_isTracker)[i]
+                Electron_jetIdx[i] = list(event.Electron_jetIdx)[i]
+                Electron_pdgId[i] = list(event.Electron_pdgId)[i]
+                # Electron_looseId[i] = list(event.Electron_looseId)[i]
+                # Electron_mediumId[i] = list(event.Electron_mediumId)[i]
+                # Electron_mediumPromptId[i] = list(event.Electron_mediumPromptId)[i]
+                # Electron_mvaId[i] = ord(list(event.Electron_mvaId)[i])
+                # Electron_tightId[i] = list(event.Electron_tightId)[i]
+                Electron_genPartFlav[i] = ord(list(event.Electron_genPartFlav)[i])
+                Electron_genPartIdx[i] = list(event.Electron_genPartIdx)[i]
                 Electron_mt[i] = sqrt(2 * Electron_pt[i] * event.MET_pt * \
                         (1 - cos(Electron_phi[i] - event.MET_phi)))
-    
+
             lep1_isMu[0] = int(l1Flav == "Muon")
             lep1_index[0] = l1Index
-            lep1_pt[0] = list(getattr(event, l1Flav+"_pt"))[l1Index]
-            lep1_eta[0] = list(getattr(event, l1Flav+"_eta"))[l1Index]
-            lep1_phi[0] = list(getattr(event, l1Flav+"_phi"))[l1Index]
-            if lep1_isMu: lep1_relIso[0] = Muon_relIso[l1Index]
-            else: lep1_relIso[0] = Electron_relIso[l1Index]
-            lep1_charge[0] = list(getattr(event, l1Flav+"_charge"))[l1Index]
-            lep1_mt[0] = sqrt(2 * lep1_pt[0] * event.MET_pt * \
-                        (1 - cos(lep1_phi[0] - event.MET_phi)))
             lep2_isMu[0] = int(l2Flav == "Muon") 
             lep2_index[0] = l2Index
-            lep2_pt[0] = list(getattr(event, l2Flav+"_pt"))[l2Index]
-            lep2_eta[0] = list(getattr(event, l2Flav+"_eta"))[l2Index]
-            lep2_phi[0] = list(getattr(event, l2Flav+"_phi"))[l2Index]
-            if lep2_isMu: lep2_relIso[0] = Muon_relIso[l2Index]
-            else: lep2_relIso[0] = Electron_relIso[l2Index]
-            lep2_charge[0] = list(getattr(event, l2Flav+"_charge"))[l2Index]
-            lep2_mt[0] = sqrt(2 * lep2_pt[0] * event.MET_pt * \
-                        (1 - cos(lep2_phi[0] - event.MET_phi)))
-    
+
             # veto (3rd lepton) checks:
             found3rdLept[0] = False
             if findingSameFlavor:
@@ -373,17 +473,49 @@ for line in redirectorFile:
                 dR_lep1_jet[0] = deltaR(event, l1Flav, l1Index, "Jet", iMaxPtJ)
                 dR_lep2_jet[0] = deltaR(event, l2Flav, l2Index, "Jet", iMaxPtJ)
     
-            met_pt[0] = event.MET_pt
-            met_phi[0] = event.MET_phi
-            mt_tot[0] = sqrt(lep1_mt[0]**2+ lep2_mt[0]**2)
-            mt_sum[0] = lep1_mt[0] + lep2_mt[0]
-            m_eff[0] = met_pt[0] + lep1_pt[0]+ lep2_pt[0]
+            MET_pt[0] = event.MET_pt
+            MET_phi[0] = event.MET_phi
+            MET_significance[0] = event.MET_significance
+            MET_sumEt[0] = event.MET_sumEt
+
+            if bool(lep1_isMu[0]): 
+                lep1_mt = Muon_mt[l1Index]
+                lep1_pt = Muon_pt[l1Index]
+            else:
+                lep1_mt = Electron_mt[l1Index]
+                lep1_pt = Electron_pt[l1Index]
+            if bool(lep2_isMu[0]): 
+                lep2_mt = Muon_mt[l2Index]
+                lep2_pt = Muon_pt[l2Index]
+            else:
+                lep2_mt = Electron_mt[l2Index]
+                lep2_pt = Electron_pt[l2Index]
+    
+            mt_tot[0] = sqrt(lep1_mt**2 + lep2_mt**2)
+            mt_sum[0] = lep1_mt + lep2_mt
+            m_eff[0] = MET_pt[0] + lep1_pt + lep2_pt
             if nJet[0] > 0: m_eff[0] += Jet_ht[0]
     
-            if isData:
-                run[0] = event.run
-                luminosityBlock[0] = event.luminosityBlock
-            else: genWeight[0] = event.genWeight
+            LHE_HT[0] = event.LHE_HT
+            LHE_HTIncoming[0] = event.LHE_HTIncoming
+            LHE_Njets[0] = ord(event.LHE_Njets)
+            LHE_Nb[0] = ord(event.LHE_Nb)
+            LHE_Nuds[0] = ord(event.LHE_Nuds)
+            LHE_Nglu[0] = ord(event.LHE_Nglu)
+            LHE_NpNLO[0] = ord(event.LHE_NpNLO)
+            LHE_NpLO[0] = ord(event.LHE_NpLO)
+            # LHEPart_pt[0] = event.LHEPart_pt
+            # LHEPart_eta[0] = event.LHEPart_eta
+            # LHEPart_phi[0] = event.LHEPart_phi
+            # LHEPart_mass[0] = event.LHEPart_mass
+            # LHEPart_pdgId[0] = event.LHEPart_pdgId
+            LHEWeight_originalXWGTUP[0] = event.LHEWeight_originalXWGTUP
+
+            PV_npvs[0] = event.PV_npvs
+            PV_npvsGood[0] = event.PV_npvsGood
+            luminosityBlock[0] = event.luminosityBlock
+            run[0] = event.run
+            genWeight[0] = event.genWeight
 
             Events.Fill()
     
